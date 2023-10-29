b527dd4e807496dbb1e171c595583045eb410b5c
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2023-10-30 01:42:42 +0530

    Created React app and Added routes

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import HomePage from "./pages/HomePage";
+import PageNotFound from './pages/PageNotFound';
+import Policy from './pages/Policy';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
