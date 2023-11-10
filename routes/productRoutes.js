2f24573929f3c2593e01fd9e7744af4cb4402a7e
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2023-11-10 11:45:00 +0530

    Product API created

---
+++ routes/productRoutes.js
@@ -0,0 +1,46 @@
+import express from "express";
+import {
+    createProductController,
+    deleteProductController,
+    getProductController,
+    getSingleProductController,
+    productPhotoController,
+    updateProductController,
+} from "../controllers/productController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+const router = express.Router();
+
+//routes
+
+//create product
+router.post(
+  "/create-product",
+  authMiddleware.requireSignIn,
+  authMiddleware.isAdmin,
+  formidable(),
+  createProductController
+);
+
+//update product
+router.put(
+  "/update-product/:pid",
+  authMiddleware.requireSignIn,
+  authMiddleware.isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get Products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete rproduct
+router.delete("/product/:pid", deleteProductController);
+
+export default router;
