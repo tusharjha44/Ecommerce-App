6353dc76a407adec7457852734a13438f2b3113e
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2023-11-18 19:47:22 +0530

    Removed warnings

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import { useParams, useNavigate } from "react-router-dom";
